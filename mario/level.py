c8ba058bafe7e19d57cd10ce431576c25a568b81
taniki <tk@deveha.com>
2017-11-26 04:08:09 +0100

    CA MARCHE LES GOWS ET LES GARS !!!!!

--- mario/level.py
+++ mario/level.py
@@ -1,16 +1,25 @@
 import midi
 import json
-
-pattern = midi.read_midifile("midi/test.mid")
-#pattern = midi.read_midifile("midi/LetItGo.mid")
+from collections import defaultdict
+# pattern = midi.read_midifile("midi/test.mid")
+pattern = midi.read_midifile("midi/LetItGo.mid")
 
 level = []
 t = 0
 
-for p in pattern:
+# keep only notes
+notes = [ { 'type': note.name, "pitch": note.data[0], 'channel': c, 'tick': note.tick } for c, p in enumerate(pattern) for note in p if note.name == "Note Off" or note.name == "Note On" ]
 
+notes2 = defaultdict(list)
+
+for note in notes:
+    notes2[str(note['channel'])].append(note)
+
+for p in pattern:
     for track in p:
         print track
+        #print track.channel
+        print track.name
         print track.tick
         if (len(track.data) == 2):
             t = t + track.tick
@@ -24,6 +33,12 @@ for p in pattern:
             }
             
             level.append(data)
-        
+
+print notes
+
 with open('level.json', 'w') as outfile:
-    json.dump(level, outfile)
+    json.dump(notes, outfile)
+
+
+with open('level2.json', 'w') as outfile:
+    json.dump(notes2, outfile)
